fb7915be1ac4efb1eda1865be667d14c87b6e381
Fiddelis <lucas.ruan@ges.inatel.br>
2025-08-18 20:42:24 -0300

    feat: create /ping/count route

--- src/c14_ping/api/routes/ping.py
+++ src/c14_ping/api/routes/ping.py
@@ -8,15 +8,19 @@ from c14_ping.schemas.ping_response import PingResponse
 
 router = APIRouter(prefix='/ping')
 
+global count_var
+count_var = 0
 
 @router.get('', response_model=PingResponse, status_code=status.HTTP_200_OK)
 async def ping():
     print("Pingou!")
+    global count_var
+    count_var += 1
 
     async def fire_and_forget():
         async with httpx.AsyncClient() as client:
             try:
-                await client.get("http://192.168.209.219:3000/pong")
+                await client.get("http://192.168.209.154:3000/pong")
             except Exception as e:
                 print("Erro na chamada fire-and-forget:", e)
 
@@ -24,3 +28,8 @@ async def ping():
     asyncio.create_task(fire_and_forget())
 
     return PingResponse()
+
+@router.get('/count', status_code=status.HTTP_200_OK)
+def count():
+    global count_var
+    return count_var
